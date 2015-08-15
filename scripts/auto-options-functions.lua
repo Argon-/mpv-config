98397bb92efe4696922ed6dda7d1cced3d2606fe
Julian <MyFakeAcc.4@googlemail.com>
2015-08-15 20:50:15 +0200

    auto-options: split into two files
    Mainly for cosmetic reasons, to be honest.
    Functions are all declared in global space as I don't feel like
    prefixing every usage of them now.

---
+++ scripts/auto-options-functions.lua
@@ -0,0 +1,104 @@
+local utils = require 'mp.utils'
+
+
+-- Select the options level appropriate for this computer
+function determine_level(o, vo, vo_opts, options)
+    -- Default level
+    local level = o.mq
+
+    -- Overwrite level from command line with --script-opts=ao-level=<level>
+    local overwrite = mp.get_opt("ao-level")
+    if overwrite then
+        if not (vo[overwrite] and vo_opts[overwrite] and options[overwrite]) then
+            print("Forced level does not exist: " .. overwrite)
+            return level
+        end
+        return overwrite
+    end
+
+    -- Call an external bash function determining whether this is a desktop or laptop
+    loc = exec({"bash", "-c", 'source "$HOME"/local/shell/location-detection && is-desktop'})
+
+    -- Something went wrong
+    if loc.error then
+        loc.status = 255
+    end
+
+    -- Desktop -> hq
+    if loc.status == 0 then
+        level = o.hq
+    -- Laptop  -> mq/lq
+    elseif loc.status == 1 then
+        level = o.mq
+        -- Go down to lq when we are on battery
+        bat = exec({"/usr/bin/pmset", "-g", "ac"})
+        if bat.stdout == "No adapter attached.\n" then
+            level = o.lq
+        end
+    elseif o.verbose then
+        print("unable to determine location, using default level: " .. level)
+    end
+
+    return level
+end
+
+
+-- Use tables `vo` and `vo_opts` to build a `vo=key1=val1:key2=val2` string
+function vo_property_string(level, vo, vo_opts)
+    local result = {}
+    for k, v in pairs(vo_opts[level]) do
+        if type(v) == "function" then
+            v = v()
+        end
+        if v and v ~= "" then
+            table.insert(result, k .. "=" ..v)
+        else
+            table.insert(result, k)
+        end
+    end
+    return vo[level] .. (next(result) == nil and "" or (":" .. table.concat(result, ":")))
+end
+
+
+-- Determine if the currently used resolution is higher than o.highres_threshold
+function is_high_res(o)
+    sp_ret = exec({"/usr/local/bin/resolution", "compare", o.highres_threshold})
+    return not sp_ret.error and sp_ret.status > 2
+end
+
+
+function exec(process)
+    p = {}
+    p.args = process
+    p_ret = utils.subprocess(p)
+    if p_ret.error and p_ret.error == "init" then
+        print("ERROR executable not found: " .. process[1])
+    end
+    return p_ret
+end
+
+
+function set_ASS(b)
+    return mp.get_property_osd("osd-ass-cc/" .. (b and "0" or "1"))
+end
+
+
+function red_border(s)
+    return set_ASS(true) .. "{\\bord1}{\\3c&H3300FF&}{\\3a&H20&}" .. s .. "{\\r}" .. set_ASS(false)
+end
+
+
+function print_status(name, value, o)
+    if not value or not o.level then
+        return
+    end
+
+    if o.err_occ then
+        print("Error setting level: " .. o.level)
+        mp.osd_message(red_border("Error setting level: ") .. o.level, o.duration * o.duration_err_mult)
+    else
+        print("Active level: " .. o.level)
+        mp.osd_message("Level: " .. o.level, o.duration)
+    end
+    mp.unobserve_property(print_status)
+end
--- scripts/auto-options.lua
+++ scripts/auto-options.lua
@@ -9,8 +9,11 @@
 -- General mpv options can be defined in the `options` table while VO
 -- sub-options are to be defined in `vo_opts`.
 --
--- One probably has to reimplement `determine_level()` since it's pretty OS and
--- user specific.
+-- To adapt it for personal use one probably has to reimplement a few things.
+-- Out of the used functions `determine_level()` is the most important one
+-- requiring adjustments since it's pretty OS as well as user specific and
+-- serves as an essential component. Other functions provide various, minor
+-- supporting functionality, e.g. for use in option-specific sub-decisions.
 
 
 -- Don't do anything when mpv was called with an explicitly passed --vo option
@@ -18,8 +21,8 @@ if mp.get_property_bool("option-info/vo/set-from-commandline") then
     return
 end
 
-local options = require 'mp.options'
-local utils = require 'mp.utils'
+local f = require 'auto-options-functions'
+local opts = require 'mp.options'
 
 local o = {
     hq = "desktop",
@@ -30,96 +33,10 @@ local o = {
     duration = 5,
     duration_err_mult = 2,
 }
-options.read_options(o)
-
--- Tables containing the options to set. Possible levels are considered keys
--- in these tables, values are specified further below.
-local vo = {}
-local vo_opts = {}
-local options = {}
+opts.read_options(o)
 
 
-function determine_level(o)
-    -- Default level
-    local level = o.mq
-
-    -- Overwrite level from command line with --script-opts=ao-level=<level>
-    local overwrite = mp.get_opt("ao-level")
-    if overwrite then
-        if not (vo[overwrite] and vo_opts[overwrite] and options[overwrite]) then
-            print("Forced level does not exist: " .. overwrite)
-            return level
-        end
-        return overwrite
-    end
-
-    -- Call an external bash function determining if this is a desktop or laptop
-    loc = {}
-    loc.args = {"bash", "-c", 'source "$HOME"/local/shell/location-detection && is-desktop'}
-    loc_ret = utils.subprocess(loc)
-
-    -- Something went wrong
-    if loc_ret.error then
-        loc_ret.status = 255
-    end
-
-    -- Desktop -> hq
-    if loc_ret.status == 0 then
-        level = o.hq
-    -- Laptop  -> mq/lq
-    elseif loc_ret.status == 1 then
-        level = o.mq
-        -- Go down to lq when we are on battery
-        bat = {}
-        bat.args = {"/usr/bin/pmset", "-g", "ac"}
-        bat_ret = utils.subprocess(bat)
-        if bat_ret.stdout == "No adapter attached.\n" then
-            level = o.lq
-        end
-    elseif o.verbose then
-        print("unable to determine location, using default level: " .. level)
-    end
-
-    return level
-end
-
-
--- Use tables `vo` and `vo_opts` to build a `vo=key1=val1:key2=val2` string
-function vo_property_string(level, vo, vo_opts)
-    local result = {}
-    for k, v in pairs(vo_opts[level]) do
-        if type(v) == "function" then
-            v = v()
-        end
-        if v and v ~= "" then
-            table.insert(result, k .. "=" ..v)
-        else
-            table.insert(result, k)
-        end
-    end
-    return vo[level] .. (next(result) == nil and "" or (":" .. table.concat(result, ":")))
-end
-
-
-function is_high_res(o)
-    sp = {}
-    sp.args = {"/usr/local/bin/resolution", "compare", o.highres_threshold}
-    sp_ret = utils.subprocess(sp)
-    return not sp_ret.error and sp_ret.status > 2
-end
-
-
-function set_ASS(b)
-    return mp.get_property_osd("osd-ass-cc/" .. (b and "0" or "1"))
-end
-
-
-function red_border(s)
-    return set_ASS(true) .. "{\\bord1}{\\3c&H3300FF&}{\\3a&H20&}" .. s .. "{\\r}" .. set_ASS(false)
-end
-
-
--- Define VO for different levels
+-- Specify a VO for each level
 
 vo = {
     [o.hq] = "opengl-hq",
@@ -128,117 +45,110 @@ vo = {
 }
 
 
--- Define VO sub-options for different levels
+-- Specify VO sub-options for different levels
 
-vo_opts[o.hq] = {
-    ["scale"]  = "ewa_lanczossharp",
-    ["cscale"] = "ewa_lanczossoft",
-    ["dscale"] = "mitchell",
-    ["tscale"] = "oversample",
-    ["scale-antiring"]  = "0.8",
-    ["cscale-antiring"] = "0.9",
+vo_opts = {
+    [o.hq] = {
+        ["scale"]  = "ewa_lanczossharp",
+        ["cscale"] = "ewa_lanczossoft",
+        ["dscale"] = "mitchell",
+        ["tscale"] = "oversample",
+        ["scale-antiring"]  = "0.8",
+        ["cscale-antiring"] = "0.9",
 
-    ["dither-depth"]        = "auto",
-    --["icc-profile-auto"]    = "",
-    ["target-prim"]         = "bt.709",
-    ["scaler-resizes-only"] = "",
-    ["sigmoid-upscaling"]   = "",
+        ["dither-depth"]        = "auto",
+        --["icc-profile-auto"]    = "",
+        ["target-prim"]         = "bt.709",
+        ["scaler-resizes-only"] = "",
+        ["sigmoid-upscaling"]   = "",
 
-    --["interpolation"]     = is_high_res(o) and "no" or "yes",
-    ["fancy-downscaling"] = "",
-    ["source-shader"]     = "~~/shaders/deband.glsl",
-    ["icc-cache-dir"]     = "~~/icc-cache",
-    ["3dlut-size"]        = "256x256x256",
-}
+        --["interpolation"]     = function () return is_high_res(o) and "no" or "yes" end,
+        ["fancy-downscaling"] = "",
+        ["source-shader"]     = "~~/shaders/deband.glsl",
+        ["icc-cache-dir"]     = "~~/icc-cache",
+        ["3dlut-size"]        = "256x256x256",
+    },
 
-vo_opts[o.mq] = {
-    ["scale"]  = "spline36",
-    ["cscale"] = "spline36",
-    ["dscale"] = "mitchell",
-    ["tscale"] = "oversample",
-    ["scale-antiring"]  = "0.8",
-    ["cscale-antiring"] = "0.9",
+    [o.mq] = {
+        ["scale"]  = "spline36",
+        ["cscale"] = "spline36",
+        ["dscale"] = "mitchell",
+        ["tscale"] = "oversample",
+        ["scale-antiring"]  = "0.8",
+        ["cscale-antiring"] = "0.9",
 
-    ["dither-depth"]        = "auto",
-    --["icc-profile-auto"]    = "",
-    ["target-prim"]         = "bt.709",
-    ["scaler-resizes-only"] = "",
-    ["sigmoid-upscaling"]   = "",
+        ["dither-depth"]        = "auto",
+        --["icc-profile-auto"]    = "",
+        ["target-prim"]         = "bt.709",
+        ["scaler-resizes-only"] = "",
+        ["sigmoid-upscaling"]   = "",
 
-    ["interpolation"]     = is_high_res(o) and "no" or "yes",
-    ["fancy-downscaling"] = "",
-    ["source-shader"]     = "~~/shaders/deband.glsl",
-}
+        ["interpolation"]     = function () return is_high_res(o) and "no" or "yes" end,
+        ["fancy-downscaling"] = "",
+        ["source-shader"]     = "~~/shaders/deband.glsl",
+    },
 
-vo_opts[o.lq] = {
-    ["scale"]  = "spline36",
-    ["dscale"] = "mitchell",
-    ["tscale"] = "oversample",
+    [o.lq] = {
+        ["scale"]  = "spline36",
+        ["dscale"] = "mitchell",
+        ["tscale"] = "oversample",
 
-    ["dither-depth"]        = "auto",
-    --["icc-profile-auto"]    = "",
-    ["target-prim"]         = "bt.709",
-    ["scaler-resizes-only"] = "",
-    ["sigmoid-upscaling"]   = "",
+        ["dither-depth"]        = "auto",
+        --["icc-profile-auto"]    = "",
+        ["target-prim"]         = "bt.709",
+        ["scaler-resizes-only"] = "",
+        ["sigmoid-upscaling"]   = "",
 
-    --["interpolation"]     = is_high_res(o) and "no" or "yes",
-}
-
-
--- Define mpv options for different levels.
--- Option names (keys) are given as strings, values as functions without parameters.
-
-options[o.hq] = {
-    ["options/vo"] = function () return vo_property_string(o.hq, vo, vo_opts) end,
-    ["options/hwdec"] = "no",
-    ["options/vd-lavc-threads"] = "16",
-}
-
-options[o.mq] = {
-    ["options/vo"] = function () return vo_property_string(o.mq, vo, vo_opts) end,
-    ["options/hwdec"] = "no",
-}
-
-options[o.lq] = {
-    ["options/vo"] = function () return vo_property_string(o.lq, vo, vo_opts) end,
-    ["options/hwdec"] = "auto",
+        --["interpolation"]     = function () return is_high_res(o) and "no" or "yes" end,
+    },
 }
 
 
--- Set all defined options for the determined level
+-- Specify general mpv options for different levels
 
-local level = determine_level(o)
-local err_occ = false
-for k, v in pairs(options[level]) do
-    if type(v) == "function" then
-        v = v()
-    end
-    success, err = mp.set_property(k, v)
-    err_occ = err_occ or not (err_occ or success)
-    if success and o.verbose then
-        print("Set '" .. k .. "' to '" .. v .. "'")
-    elseif o.verbose then
-        print("Failed to set '" .. k .. "' to '" .. v .. "'")
-        print(err)
-    end
-end
+options = {
+    [o.hq] = {
+        ["options/vo"] = function () return vo_property_string(o.hq, vo, vo_opts) end,
+        ["options/hwdec"] = "no",
+        ["options/vd-lavc-threads"] = "16",
+    },
+
+    [o.mq] = {
+        ["options/vo"] = function () return vo_property_string(o.mq, vo, vo_opts) end,
+        ["options/hwdec"] = "no",
+    },
+
+    [o.lq] = {
+        ["options/vo"] = function () return vo_property_string(o.lq, vo, vo_opts) end,
+        ["options/hwdec"] = "auto",
+    },
+}
 
 
 -- Print status information to VO window and terminal
 
-function print_status(name, value)
-    if not value then
-        return
-    end
+mp.observe_property("vo-configured", "bool",
+                    function (name, value) print_status(name, value, o) end)
 
-    if err_occ then
-        print("Error setting level: " .. level)
-        mp.osd_message(red_border("Error setting level: ") .. level, o.duration * o.duration_err_mult)
-    else
-        print("Active level: " .. level)
-        mp.osd_message("Level: " .. level, o.duration)
+
+-- Set all defined options for the determined level
+
+function main()
+    o.level = determine_level(o, vo, vo_opts, options)
+    o.err_occ = false
+    for k, v in pairs(options[o.level]) do
+        if type(v) == "function" then
+            v = v()
+        end
+        success, err = mp.set_property(k, v)
+        o.err_occ = o.err_occ or not (o.err_occ or success)
+        if success and o.verbose then
+            print("Set '" .. k .. "' to '" .. v .. "'")
+        elseif o.verbose then
+            print("Failed to set '" .. k .. "' to '" .. v .. "'")
+            print(err)
+        end
     end
-    mp.unobserve_property(print_status)
 end
 
-mp.observe_property("vo-configured", "bool", print_status)
+main()
