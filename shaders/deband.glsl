d233e3d181e4e124eaa8a972290658810bd94e07
Julian <MyFakeAcc.4@googlemail.com>
2015-09-14 03:05:49 +0200

    deband: new deband shader from mpv repo

--- shaders/deband.glsl
+++ shaders/deband.glsl
@@ -1,21 +1,47 @@
-// vim: set ft=glsl:
+// GLSL debanding shader, use as: source-shader=path/to/deband.glsl
+// (Loosely based on flash3kyuu_deband, but expanded to multiple iterations)
 
-// roughly corresponds to f3kdb parameters, which this algorithm is
-// loosely inspired by
-#define threshold 64
-#define range     16
-#define grain     24
+//------------ Configuration section ------------
+// The threshold of difference below which a pixel is considered to be part of
+// a gradient. Higher = more debanding, but setting it too high diminishes image
+// details.
+#define THRESHOLD 64
 
-float rand(vec2 co){
-    return fract(sin(dot(co.yx, vec2(12.9898,78.233))) * 43758.5453);
-}
+// The range (in source pixels) at which to sample for neighbours. Higher values
+// will find more gradients, but lower values will deband more aggressively.
+#define RANGE 8
 
-vec4 sample(sampler2D tex, vec2 pos, vec2 tex_size)
+// The number of debanding iterations to perform. Each iteration samples from
+// random positions, so increasing the number of iterations is likely to
+// increase the debanding quality. Conversely, it slows the shader down.
+// (Each iteration will use a multiple of the configured RANGE, and a
+// successively lower THRESHOLD - so setting it much higher has little effect)
+#define ITERATIONS 4
+
+// (Optional) Add some extra noise to the image. This significantly helps cover
+// up remaining banding and blocking artifacts, at comparatively little visual
+// quality. Higher = more grain. Setting it to 0 disables the effect.
+//#define GRAIN 48
+#define GRAIN 32
+
+// Note: If performance is too slow, try eg. RANGE=16 ITERATIONS=2. In general,
+// an increase in the number of ITERATIONS should roughly correspond to a
+// decrease in RANGE and perhaps an increase in THRESHOLD.
+//------------ End of configuration ------------
+
+// Wide usage friendly PRNG, shamelessly stolen from a GLSL tricks forum post
+float mod289(float x)  { return x - floor(x / 289.0) * 289.0; }
+float permute(float x) { return mod289((34.0*x + 1.0) * x); }
+float rand(float x)    { return fract(x / 41.0); }
+
+// Helper: Calculate a stochastic approximation of the avg color around a pixel
+vec4 average(sampler2D tex, vec2 pos, float range, inout float h)
 {
-    // Compute a random angle and distance
-    float dist = rand(pos + vec2(random)) * range;
+    // Compute a random rangle and distance
+    float dist = rand(h) * range;     h = permute(h);
+    float dir  = rand(h) * 6.2831853; h = permute(h);
+
     vec2 pt = dist / image_size;
-    float dir = rand(pos.yx - vec2(random)) * 6.2831853;
     vec2 o = vec2(cos(dir), sin(dir));
 
     // Sample at quarter-turn intervals around the source pixel
@@ -25,18 +51,35 @@ vec4 sample(sampler2D tex, vec2 pos, vec2 tex_size)
     ref[2] = texture(tex, pos + pt * vec2(-o.x, -o.y));
     ref[3] = texture(tex, pos + pt * vec2( o.y, -o.x));
 
-    // Average and compare with the actual sample
-    vec4 avg = cmul*(ref[0] + ref[1] + ref[2] + ref[3])/4.0;
-    vec4 col = cmul*texture(tex, pos);
-    vec4 diff = abs(col - avg);
+    // Return the (normalized) average
+    return cmul*(ref[0] + ref[1] + ref[2] + ref[3])/4.0;
+}
 
-    // Use the average if below the threshold
-    col = mix(avg, col, greaterThan(diff, vec4(threshold/16384.0)));
+vec4 sample(sampler2D tex, vec2 pos, vec2 tex_size)
+{
+    float h;
+    // Initialize the PRNG by hashing the position + a random uniform
+    vec3 m = vec3(pos, random) + vec3(1.0);
+    h = permute(permute(permute(m.x)+m.y)+m.z);
+
+    // Sample the source pixel
+    vec4 col = cmul*texture(tex, pos);
+
+    for (int i = 1; i <= ITERATIONS; i++) {
+        // Use the average instead if the difference is below the threshold
+        vec4 avg = average(tex, pos, i*RANGE, h);
+        vec4 diff = abs(col - avg);
+        col = mix(avg, col, greaterThan(diff, vec4(THRESHOLD/(i*16384.0))));
+    }
 
     // Add some random noise to the output
-    vec3 noise = vec3(rand(2*pos + vec2(random)),
-                      rand(3*pos + vec2(random)),
-                      rand(4*pos + vec2(random)));
-    col.rgb += (grain/8192.0) * (noise - vec3(0.5));
+    vec3 noise;
+    noise.x = rand(h); h = permute(h);
+    noise.y = rand(h); h = permute(h);
+    noise.z = rand(h); h = permute(h);
+    col.rgb += (GRAIN/8192.0) * (noise - vec3(0.5));
+
     return col;
 }
+
+// vim: set ft=glsl:
