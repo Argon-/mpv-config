2d9e81abd8a3fe4dbeae35e2ebc773f428096a39
Julian <MyFakeAcc.4@googlemail.com>
2018-09-30 16:11:18 +0200

    Update 30.09.2018 (2)

--- scripts/auto-audio-device.lua
+++ scripts/auto-audio-device.lua
@@ -5,6 +5,7 @@ local auto_change = true
 local av_map = {
     ["DELL U2312HM"] = "coreaudio/AppleUSBAudioEngine:FiiO:DigiHug USB Audio:1a160000:3", -- FiiO DAC
     ["SONY TV  *00"] = "coreaudio/AppleGFXHDAEngineOutputDP:0:{D94D-8204-01010101}", -- HDMI
+    ["Color LCD"]    = "coreaudio/AppleHDAEngineOutput:1B,0,1,1:0", -- Built-in
 }
 
 function set_audio_device(obs_display)
@@ -18,19 +19,17 @@ function set_audio_device(obs_display)
         return
     end
 
-    local current_adev = mp.get_property("audio-device", "auto")
     local new_adev = av_map[display[1]]
-    if new_adev and type(new_adev) == "string" and new_adev ~= current_adev then
+    local current_adev = mp.get_property("audio-device", "auto")
+    if not new_adev then
+        new_adev = "auto"
+    end
+    if new_adev ~= current_adev then
         mp.osd_message("Audio device: " .. new_adev)
-        mp.set_property("audio-device", new_adev)    
+        mp.set_property("audio-device", new_adev)
     end
 end
 
 mp.add_key_binding("", "set-auto-audio-device", function() set_audio_device(nil) end)
 mp.add_key_binding("", "toggle-auto-audio-device", function() auto_change = not auto_change end)
-
-mp.observe_property("display-names", "native", 
-    function(name, value)
-        set_audio_device(value)
-        print(name .. ": " .. tostring(value[1])) 
-    end)
+mp.observe_property("display-names", "native", function(name, value) set_audio_device(value) end)
